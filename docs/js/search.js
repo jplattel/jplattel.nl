d44b07cee1cf6fa08577f532271874d8ddef5714
Joost Plattel <jsplattel@gmail.com>
2019-10-19 12:28:18 +0200

    rebuilding site Sat Oct 19 12:28:17 CEST 2019

--- docs/js/search.js
+++ docs/js/search.js
@@ -1,6 +1,6 @@
-function initSearchIndex() {
+function initSearchIndex(searchFile) {
     // this file is built by the Grunt task, and
-    $.getJSON('index.json')
+    $.getJSON(searchFile)
       .done(function(documents) {
         pagesIndex = documents;
         searchIndex = lunr(function() {
@@ -24,5 +24,32 @@ function initSearchIndex() {
       }
     );
   }
-  
-  initSearchIndex();
+
+// this function will parse the query_string, which will you
+// to run queries like "title:lunr" (search the title field),
+// "lunr^10" (boost hits with this term by a factor 10) or
+// "lunr~2" (will match anything within an edit distance of 2,
+// i.e. "losr" will also match)
+function simpleSearchSite(query_string) {
+    return searchIndex.search(query_string).map(function(result) {
+        return pagesIndex.filter(function(page) {
+            return page.href === result.ref;
+        })[0];
+    });
+}
+
+// I want a typeahead search, so if a user types a query like
+// "pyth", it should show results that contain the word "Python",
+// rather than just the entire word.
+function searchSite(query_string) {
+    return searchIndex.query(function(q) {
+        // look for an exact match and give that a massive positive boost
+        q.term(query_string, { usePipeline: true, boost: 100 });
+        // prefix matches should not use stemming, and lower positive boost
+        q.term(query_string, { usePipeline: false, boost: 10, wildcard: lunr.Query.wildcard.TRAILING });
+    }).map(function(result) {
+        return pagesIndex.filter(function(page) {
+        return page.href === result.ref;
+        })[0];
+    });
+}
