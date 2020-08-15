193a5f55a4c9cd1dc7e6cf8a2763b136ee6e756b
Joost Plattel <jsplattel@gmail.com>
2020-08-15 22:43:59 +0200

    Rebuilding site: Sat Aug 15 22:43:58 CEST 2020

--- docs/js/search.js
+++ docs/js/search.js
@@ -1,55 +1,154 @@
-function initSearchIndex(searchFile) {
-    // this file is built by the Grunt task, and
-    $.getJSON(searchFile)
-      .done(function(documents) {
-        pagesIndex = documents;
-        searchIndex = lunr(function() {
-          this.field('title');
-          this.field('categories');
-          this.field('content');
-          this.ref('href');
-  
-          // This will add all the documents to the index. This is
-          // different compared to older versions of Lunr, where
-          // documents could be added after index initialisation
-          for (var i = 0; i < documents.length; ++i) {
-            this.add(documents[i])
-          }
-        });
-        console.log("Setting up search")
-      })
-      .fail(function(jqxhr, textStatus, error) {
-        var err = textStatus + ', ' + error;
-        console.error('Error getting index file:', err);
-      }
-    );
+// A local search script with the help of
+// [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
+// Copyright (C) 2015
+// Joseph Pan <http://github.com/wzpan>
+// Shuhao Mao <http://github.com/maoshuhao>
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+// Modified by:
+// Pieter Robberechts <http://github.com/probberechts>
+
+/*exported searchFunc*/
+var searchFunc = function(path, searchId, contentId) {
+
+  function stripHtml(html) {
+    html = html.replace(/<style([\s\S]*?)<\/style>/gi, "");
+    html = html.replace(/<script([\s\S]*?)<\/script>/gi, "");
+    html = html.replace(/<figure([\s\S]*?)<\/figure>/gi, "");
+    html = html.replace(/<\/div>/ig, "\n");
+    html = html.replace(/<\/li>/ig, "\n");
+    html = html.replace(/<li>/ig, "  *  ");
+    html = html.replace(/<\/ul>/ig, "\n");
+    html = html.replace(/<\/p>/ig, "\n");
+    html = html.replace(/<br\s*[\/]?>/gi, "\n");
+    html = html.replace(/<[^>]+>/ig, "");
+    return html;
   }
 
-// this function will parse the query_string, which will you
-// to run queries like "title:lunr" (search the title field),
-// "lunr^10" (boost hits with this term by a factor 10) or
-// "lunr~2" (will match anything within an edit distance of 2,
-// i.e. "losr" will also match)
-function simpleSearchSite(query_string) {
-    return searchIndex.search(query_string).map(function(result) {
-        return pagesIndex.filter(function(page) {
-            return page.href === result.ref;
-        })[0];
-    });
-}
+  function getAllCombinations(keywords) {
+    var i, j, result = [];
 
-// I want a typeahead search, so if a user types a query like
-// "pyth", it should show results that contain the word "Python",
-// rather than just the entire word.
-function searchSite(query_string) {
-    return searchIndex.query(function(q) {
-        // look for an exact match and give that a massive positive boost
-        q.term(query_string, { usePipeline: true, boost: 100 });
-        // prefix matches should not use stemming, and lower positive boost
-        q.term(query_string, { usePipeline: false, boost: 10, wildcard: lunr.Query.wildcard.TRAILING });
-    }).map(function(result) {
-        return pagesIndex.filter(function(page) {
-        return page.href === result.ref;
-        })[0];
-    });
-}
+    for (i = 0; i < keywords.length; i++) {
+        for (j = i + 1; j < keywords.length + 1; j++) {
+            result.push(keywords.slice(i, j).join(" "));
+        }
+    }
+    return result;
+  }
+
+  $.ajax({
+    url: path,
+    dataType: "xml",
+    success: function(xmlResponse) {
+      // get the contents from search data
+      var datas = $("entry", xmlResponse).map(function() {
+        return {
+          title: $("title", this).text(),
+          content: $("content", this).text(),
+          url: $("link", this).attr("href")
+        };
+      }).get();
+
+      var $input = document.getElementById(searchId);
+      if (!$input) { return; }
+      var $resultContent = document.getElementById(contentId);
+
+      $input.addEventListener("input", function(){
+        var resultList = [];
+        var keywords = getAllCombinations(this.value.trim().toLowerCase().split(" "))
+          .sort(function(a,b) { return b.split(" ").length - a.split(" ").length; });
+        $resultContent.innerHTML = "";
+        if (this.value.trim().length <= 0) {
+          return;
+        }
+        // perform local searching
+        datas.forEach(function(data) {
+          var matches = 0;
+          if (!data.title || data.title.trim() === "") {
+            data.title = "Untitled";
+          }
+          var dataTitle = data.title.trim().toLowerCase();
+          var dataContent = stripHtml(data.content.trim());
+          var dataUrl = data.url;
+          var indexTitle = -1;
+          var indexContent = -1;
+          var firstOccur = -1;
+          // only match artiles with not empty contents
+          if (dataContent !== "") {
+            keywords.forEach(function(keyword) {
+              indexTitle = dataTitle.indexOf(keyword);
+              indexContent = dataContent.indexOf(keyword);
+
+              if( indexTitle >= 0 || indexContent >= 0 ){
+                matches += 1;
+                if (indexContent < 0) {
+                  indexContent = 0;
+                }
+                if (firstOccur < 0) {
+                  firstOccur = indexContent;
+                }
+              }
+            });
+          }
+          // show search results
+          if (matches > 0) {
+            var searchResult = {};
+            searchResult.rank = matches;
+            searchResult.str = "<li><a href='"+ dataUrl +"' class='search-result-title'>"+ dataTitle +"</a>";
+            if (firstOccur >= 0) {
+              // cut out 100 characters
+              var start = firstOccur - 20;
+              var end = firstOccur + 80;
+
+              if(start < 0){
+                start = 0;
+              }
+
+              if(start == 0){
+                end = 100;
+              }
+
+              if(end > dataContent.length){
+                end = dataContent.length;
+              }
+
+              var matchContent = dataContent.substr(start, end);
+
+              // highlight all keywords
+              var regS = new RegExp(keywords.join("|"), "gi");
+              matchContent = matchContent.replace(regS, function(keyword) {
+                return "<em class=\"search-keyword\">"+keyword+"</em>";
+              });
+
+              searchResult.str += "<p class=\"search-result\">" + matchContent +"...</p>";
+            }
+            searchResult.str += "</li>";
+            resultList.push(searchResult);
+          }
+        });
+        resultList.sort(function(a, b) {
+            return b.rank - a.rank;
+        });
+        var result ="<ul class=\"search-result-list\">";
+        for (var i = 0; i < resultList.length; i++) {
+          result += resultList[i].str;
+        }
+        result += "</ul>";
+        $resultContent.innerHTML = result;
+      });
+    }
+  });
+};
