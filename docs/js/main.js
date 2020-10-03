9984b884752cb3dbbff2765f6e95d76a5e6cbd41
Joost Plattel <jsplattel@gmail.com>
2020-10-03 21:00:44 +0200

    Rebuilding site: Sat Oct  3 21:00:43 CEST 2020

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,47 +1,9 @@
-/**
- * Sets up Justified Gallery.
- */
-if (!!$.prototype.justifiedGallery) {
-  var options = {
-    rowHeight: 140,
-    margins: 4,
-    lastRow: "justify"
-  };
-  $(".article-gallery").justifiedGallery(options);
-}
+// Button
+var toggle = document.querySelectorAll('#header > #nav > ul > .icon')[0];
 
-$(document).ready(function() {
-  /**
-   * Shows the responsive navigation menu on mobile.
-   */
-  $("#header > #nav > ul > .icon").click(function() {
-    console.log("clicked")
-    $("#header > #nav > ul").toggleClass("responsive");
-  });
+// Element to toggle
+toggle.addEventListener('click', function(e) {
+  document.querySelectorAll("#header > #nav > ul")[0].classList.toggle("responsive");
+}, false);
 
-  // // var menu = $("#menu");
-  // // var nav = $("#menu > #nav");
-  // // var menuIcon = $("#menu-icon, #menu-icon-tablet");
 
-  // /**
-  //  * Display the menu on hi-res laptops and desktops.
-  //  */
-  // if ($(document).width() >= 1440) {
-  //   menu.css("visibility", "visible");
-  //   menuIcon.addClass("active");
-  // }
-
-  // /**
-  //  * Display the menu if the menu icon is clicked.
-  //  */
-  // menuIcon.click(function() {
-  //   if (menu.css("visibility") === "hidden") {
-  //     menu.css("visibility", "visible");
-  //     menuIcon.addClass("active");
-  //   } else {
-  //     menu.css("visibility", "hidden");
-  //     menuIcon.removeClass("active");
-  //   }
-  //   return false;
-  // });
-});
